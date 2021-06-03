e8c7b4964a40e8f69a926593465d72e169a3138d
Tom Howard <thomaswhoward@gmail.com>
2021-06-02 20:10:19 -0400

    MAIN LRU cache 2.0 simple linked list add and get with refreshed queue.

---
+++ LRUCache2.js
@@ -0,0 +1,58 @@
+class LRULinkedList {
+    constructor () {
+        this.first = null;
+        this.last = null;
+        this.count = 0;
+        this.map = new Map();
+    }
+
+    add (kV) {
+        var node = new Node(this.first, kV[0], kV[1]);
+        if (this.first == null) {
+            this.last = node;
+        } else {
+            this.map.set(this.first.key, node);
+        }
+        this.first = node;
+    }
+
+    get (key) {
+        if (this.first && this.first.key === key) {
+            return this.first.value;
+        }
+
+        var parent = this.map.get(key);
+        if (parent !== undefined) {
+            this.map.delete(key);
+            var node = parent.next;
+            if (node != null) {
+                var value = node.value;
+                parent.next = node.next;
+                if (!parent.next) {
+                    this.last = parent;
+                }
+                this.add([key, value]);
+                return value;
+            }
+        }
+    }
+}
+
+class Node {
+    constructor (next, key, value) {
+        this.next = next;
+        this.key = key;
+        this.value = value;
+    }
+}
+
+// we should be mapping the key to the parent node, so we can find the node holding the value (stored in k-v form)
+// the paren
+
+var ll = new LRULinkedList();
+ll.add([1, 1]);
+ll.add([2, 2]);
+ll.add([3, 3]);
+console.log(ll.map);
+console.log(ll.get(1));
+console.log(ll.map);
